dde0af4c3687486f95fd8e50c31eef09e92dcfed
skomposzczet <jkalecinski01@gmail.com>
2021-12-02 11:19:10 +0100

    solved part 1 od day 1

---
+++ day2/part1/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define INPUTFILE "../input"
+
+struct Coords
+{
+    Coords() :x{0}, y{0} {}
+    int x;
+    int y;
+
+    void (Coords::*_add)(int);
+    void addx(int num) {x+=num;}
+    void addy(int num) {y+=num;}
+
+
+};
+
+int main()
+{
+    std::ifstream file(INPUTFILE);
+    if (!file.is_open())
+    {
+        std::cout << "file not found" << std::endl;
+        return 1;
+    }
+
+    Coords coords;
+    std::string buff;
+
+    while(getline(file, buff))
+    {   
+        int num = std::stoi(buff.substr(buff.find_last_of(' ')+1,1));
+        switch (buff.at(0))
+        {
+            case 'f':
+                coords.addx(num);
+                break;
+            case 'u':
+                coords.addy(-num);
+                break;
+            case 'd':
+                coords.addy(num);
+                break;
+        }
+    }
+    
+    std::cout << coords.y * coords.x << std::endl;
+}
